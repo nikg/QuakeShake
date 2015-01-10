26edb26f41739e9002e55d4616d3e1c18c81bcce
NikG <nikg@live.ca>
2015-01-10 09:03:50 -0800

    added handling/logging of redis connect error

--- app.js
+++ app.js
@@ -13,21 +13,33 @@ subScnls.key = "hawks3Z";
 //subScnls.redisPort = 6379;
 subScnls.redisHost = "products01.ess.washington.edu";
 subScnls.redisPort = 32109;
-subScnls.port = 2112; //port browser connects to
+subScnls.port = 2112; //socket.io port
 
 var io = require('socket.io')(subScnls.port); //port connection for client
-var sub = redis.createClient(subScnls.redisPort, subScnls.redisHost);
-sub.subscribe(subScnls.key);//subscribe to Pub channel
 io.on('connection', function(client){ 
-  sub.on('message', function(channel, msg) {
-    // console.log("from channel: " + channel + " msg: " + msg);
-    console.log("msg.length: " + msg.length );
-    client.send(msg);
-  });
-      
-    client.on('disconnect', function() {
-         //don't do this
-        // sub.quit();
-    });
-  
+	var sub = redis.createClient(subScnls.redisPort, subScnls.redisHost);
+	sub.subscribe(subScnls.key);//subscribe to Pub channel
+	sub.on('message', function(channel, msg) {
+		// console.log("from channel: " + channel + " msg: " + msg);
+		console.log("msg.length: " + msg.length );
+		client.send(msg);
+	});
+
+	// handle Redis 
+	sub.on('connect'     , log('connect'));
+	sub.on('reconnecting', log('reconnecting'));
+	sub.on('error'       , log('error'));
+	sub.on('end'         , log('end'));
+
+	client.on('disconnect', function() {
+	     //don't do this
+	    // sub.quit();
+	});
+
 });
+
+function log(type) {
+    return function() {
+        console.log(type, arguments);
+    }
+}
