e0fc22c969310cdfe3492f985024bd53e83506c1
NikG <nikg@live.ca>
2015-01-10 10:52:40 -0800

    with express now attaching to http port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-//Subscription side of redis pub/sub
 //To start get channel name for output of quakeShakePub
 // node server/quakeShakeSub --channel=someChannelName, --redisHost=someRedisHost, --redisPort=redisport
 
 var http = require('http');
-// var sockjs = require('sockjs');
 var redis = require('redis');
+var  express = require('express'),
+
+app = express();
+app.use(express.static(__dirname + '/public'));
 
 /* inline config details */
 var subScnls={};
@@ -15,26 +17,29 @@ subScnls.redisHost = "products01.ess.washington.edu";
 subScnls.redisPort = 32109;
 subScnls.port = 8080; //socket.io port
 
-var io = require('socket.io')(subScnls.port); //port connection for client
-io.on('connection', function(client){ 
-	var sub = redis.createClient(subScnls.redisPort, subScnls.redisHost);
-	sub.subscribe(subScnls.key);//subscribe to Pub channel
-	sub.on('message', function(channel, msg) {
-		// console.log("from channel: " + channel + " msg: " + msg);
-		console.log("msg.length: " + msg.length );
-		client.send(msg);
-	});
+var server = http.createServer(app);
+server.listen(process.env.PORT || subScnls.port);
 
-	// handle Redis 
-	sub.on('connect'     , log('connect'));
-	sub.on('reconnecting', log('reconnecting'));
-	sub.on('error'       , log('error'));
-	sub.on('end'         , log('end'));
+var io = require('socket.io')(server); //port connection for client
+io.on('connection', function(client){
+    var sub = redis.createClient(subScnls.redisPort, subScnls.redisHost);
+    sub.subscribe(subScnls.key);//subscribe to Pub channel
+    sub.on('message', function(channel, msg) {
+            // console.log("from channel: " + channel + " msg: " + msg);
+            console.log("msg.length: " + msg.length );
+            client.send(msg);
+    });
 
-	client.on('disconnect', function() {
-	     //don't do this
-	    // sub.quit();
-	});
+    // handle Redis
+    sub.on('connect'     , log('connect'));
+    sub.on('reconnecting', log('reconnecting'));
+    sub.on('error'       , log('error'));
+    sub.on('end'         , log('end'));
+
+    client.on('disconnect', function() {
+         //don't do this
+        // sub.quit();
+    });
 
 });
 
