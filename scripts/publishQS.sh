e88cb5584ae90d9156cda7db4693e7ea8a00e90c
Jose Miguel Parrella <j@bureado.com>
2015-01-11 01:26:36 +0000

    Implementing arrays for multiple websockets

--- scripts/publishQS.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/node /etc/QuakeShake/server/quakeShakePub.js config=$1 >> /var/log/quakeshake-$1.log 2>&1 &
--- scripts/subscribeQS.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/node /etc/QuakeShake/server/quakeShakeSub.js config=$1 >> /var/log/quakeshakeSub-$1.log 2>&1 &
