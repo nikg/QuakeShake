d5e782dc099de6f0b464bea6fbc3809a6c8380b4
Jose Miguel Parrella <j@bureado.com>
2015-01-11 02:15:14 +0000

    Adding PID to console.log outputs

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ subScnls.port = 8080;
 
 // Initialize servers and application
 var app = express();
-app.use(express.static(__dirname + '/client')); // Change to /public in production
+app.use(express.static(__dirname + '/public')); // Change to /public in production
 var server = http.createServer(app);
 server.listen(process.env.PORT || subScnls.port); // Azure Web Sites sets env.PORT, otherwise use config
 var io = require('socket.io')(server); // This attaches the websockets to the previously created server
@@ -31,7 +31,7 @@ io.on('connection', function(client){
 	allSocks[connectCounter] = client; // store socket in array object
 
 	connectCounter++;
-	console.log("ws client connected. total: " + connectCounter);
+	console.log("[" + process.pid + "] ws client connected. total: " + connectCounter);
 
 	client.on('disconnect', function() {
 		delete allSocks[ws.id];
@@ -42,10 +42,10 @@ io.on('connection', function(client){
 
 // New redis message
 sub.on('message', function(channel, msg) {
-	console.log("msg.length: " + msg.length );
+	console.log("[" + process.pid + "] msg.length: " + msg.length );
 	for(var key in allSocks) {
 		allSocks[key].send(msg);
-		console.log("Sent msg to socket " + allSocks[key].qsid);
+		console.log("[" + process.pid + "] sent msg to socket " + allSocks[key].qsid);
 	}
 });
 
